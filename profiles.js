beaa8d565da7e401fb2fa7075200d1d918fc6727
nancyjangra07 <nan.jangra11@gmail.com>
2026-04-06 15:18:55 +0530

    Create profiles.js

---
+++ profiles.js
@@ -0,0 +1,14 @@
+ let profiles = [
+  {
+    name: "Aman",
+    role: "Frontend Developer",
+    skills: ["HTML", "CSS", "React"],
+    bio: "Love building UI"
+  },
+  {
+    name: "Riya",
+    role: "Backend Developer",
+    skills: ["Node.js", "MongoDB"],
+    bio: "API specialist"
+  }
+];
